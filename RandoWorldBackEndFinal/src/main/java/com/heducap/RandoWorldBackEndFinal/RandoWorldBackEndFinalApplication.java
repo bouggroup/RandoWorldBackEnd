21d65a439a8cf90905ee487961f7422e0fcf910a
boug <mustapha.bouglala@capgemini.com>
2020-03-11 17:05:12 +0100

    First commit

---
+++ RandoWorldBackEndFinal/src/main/java/com/heducap/RandoWorldBackEndFinal/RandoWorldBackEndFinalApplication.java
@@ -0,0 +1,13 @@
+package com.heducap.RandoWorldBackEndFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandoWorldBackEndFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandoWorldBackEndFinalApplication.class, args);
+	}
+
+}
---
+++ RandoWorldBackEndFinal/src/test/java/com/heducap/RandoWorldBackEndFinal/RandoWorldBackEndFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heducap.RandoWorldBackEndFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandoWorldBackEndFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
