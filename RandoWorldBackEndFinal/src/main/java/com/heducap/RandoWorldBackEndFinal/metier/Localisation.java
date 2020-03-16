3b04e75246c8f8b540960bd1a13b38a27dfe4724
boug <mustapha.bouglala@capgemini.com>
2020-03-16 15:54:51 +0100

    mise a jour de localisation

--- RandoWorldBackEndFinal/src/main/java/com/heducap/RandoWorldBackEndFinal/metier/Localisation.java
+++ RandoWorldBackEndFinal/src/main/java/com/heducap/RandoWorldBackEndFinal/metier/Localisation.java
@@ -1,10 +1,11 @@
 package com.heducap.RandoWorldBackEndFinal.metier;
 
 
-import java.time.LocalDateTime;
-import java.util.Set;
-
-import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.vividsolutions.jts.geom.Geometry;
